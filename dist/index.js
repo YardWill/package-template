88c0f60d83933814b3bab756969a070b738da533
YardWill <zjwengyidong@outlook.com>
2018-08-16 09:54:08 +0800

    addEslintConfig

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sum = function (a, b) { return a + b; };
+var a = {
+    s: 1,
+    as: 1,
+    asd: 12 + "12",
+};
+// a.sss = 2;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 export const sum = (a, b) => a + b;
 
+const a = {
+  s: 1,
+  as: 1,
+  asd: `${12}12`,
+};
+
+// a.sss = 2;
